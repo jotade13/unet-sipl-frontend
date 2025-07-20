69e0dd540b0bcf87164fd83956cd2a7cafb60399
CesarMC-Port <morecontrol64@gmail.com>
2025-07-20 18:31:37 -0400

    new detais

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import { Link } from 'react-router'
 import { useForm } from "react-hook-form"
-import { Input } from "../components/Input"
+import { Input, Form } from "antd"
 
 function Login() {
 
   const { register, handleSubmit } = useForm()
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
 
   return (
     <section className='flex justify-center flex-row items-center w-full h-[100vh]'>
@@ -14,18 +18,33 @@ function Login() {
         <div className='flex flex-col items-center justify-center' style={{borderRadius:'20px', border:'1px solid #2374a7', padding:'20px'}}>
           <h1 className='text-[50px] font-bold text-center'>Gestion de equipo</h1>
           {/* formulario */}
-          <form onSubmit={handleSubmit()}>
-            <Input label="Email" register={register} required />
-            <Input label="Password" type="password" register={register} required />
-          </form>
+          <div className='w-full h-auto px-[20px]'>
+            <Form
+              layout='vertical'
+              className='w-full'
+              // style={{ maxWidth: 600 }}
+              onFinish={onFinish}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: 'Por favor agrega tu email' }]}
+              >
+                <Input />
+              </Form.Item>
+              <div className='flex justify-center items-center'>
+                <button
+                  type="submit" 
+                  style={{borderRadius:'20px',background:'#2346a7'}} 
+                  className='text-white flex justify-center items-center p-[15px] text-[20px] hover-button'
+                >
+                    Continuar
+                </button>
+              </div>
+            </Form>
+          </div>
           {/* cierre formulario */}
-          <button
-            type="submit" 
-            style={{borderRadius:'20px',background:'#2346a7'}} 
-            className='text-white flex justify-center items-center p-[15px] text-[20px] hover-button'
-          >
-              Continuar
-          </button>
         </div>
         <Link to="../register" className='text-[20px] pt-[40px]'>Crear una nueva cuenta</Link>
       </div>
