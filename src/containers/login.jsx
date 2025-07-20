4bc799a26cdf36229cb6edd878f5364a58e23d4b
Jose Mora <jdmds13@gmail.com>
2025-07-20 17:41:57 -0400

    creacion de campos del login y componente Input

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useForm } from "react-hook-form"
+import { Input } from "../components/Input"
 
 function Login() {
 
+  const { register, handleSubmit } = useForm()
+
   return (
     <>
-      
+      <h1 className='text-white'>haaola</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input label="Email" register={register} required />
+        <Input label="Password" register={register} required />
+      </form>
     </>
   )
 }
