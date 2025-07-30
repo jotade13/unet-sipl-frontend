5f4b42e340dc37be92f1ee9b5300700abd3d5c35
CesarMC-Port <morecontrol64@gmail.com>
2025-07-29 22:27:52 -0400

    new details

--- src/containers/dashboardTareas.jsx
+++ src/containers/dashboardTareas.jsx
@@ -1,10 +1,79 @@
 
 import {useCallback, useEffect, useState} from 'react'
+import { getTasks } from '../services/task-services';
+import { notification, Input } from 'antd';
 
 function DashboardTareas() {
-    
+    const [tasks, setTasks] = useState([])
+    const [status, setStatus] = useState(false)
 
-    return <div>Hola</div>
+    const getTask = useCallback(async () => {
+        let data = await getTasks((message) => {
+            if(message){
+                notification.error({message});
+            }
+        });
+        setTasks(data.data)
+    },[])
+
+    useEffect(() => {
+        getTask();
+    },[])
+
+    return <div className='w-full h-[100vh]'>
+        <div className='w-full h-[90px] border border-gray-200 border-solid shadow-sm hover:shadow-md transition-shadow duration-200 flex justify-between items-center'>
+            <div className='w-auto ml-[40px] font-bold text-[25px]'>Control de tareas JCP++</div>
+            <div className='w-auto mr-[40px]'>Nombre Apellido</div>
+        </div>
+        <div className='w-full h-auto flex flex-col justify-center items-center'>
+            <div className='w-[60%] h-auto flex row '>
+                <div className='w-[50%] radius m-[50px] p-[20px] h-auto flex flex-col border-gray-200 border-solid shadow-sm hover:shadow-md transition-shadow duration-200'>
+                    <h1 className='font-bold text-[30px] w-full text-center p-20px'>Tareas Completadas</h1>
+                    <h1 className='font-bold text-[30px] w-full text-center p-20px'>0</h1>
+                </div>
+                <div className='w-[50%] radius m-[50px] p-[20px] h-auto flex flex-col border-gray-200 border-solid shadow-sm hover:shadow-md transition-shadow duration-200'>
+                    <h1 className='font-bold block text-[30px] w-full text-center p-20px'>Tareas Pendientes</h1>
+                    <h1 className='font-bold block text-[30px] w-full text-center p-20px'>0</h1>
+                </div>
+            </div>
+        </div>
+        <div className='w-full h-auto flex row gap-5 justify-center items-center'>
+            <div className='w-[60%] flex row gap-[20px]'>
+                <div className='w-[60%]'>
+                    <Input style={{height:'100%', borderRadius:'20px'}}/>
+                </div>
+                <div 
+                    onClick={() => setStatus('completed')}
+                    style={{cursor:'pointer'}}
+                    className={status === 'completed' 
+                        ? 'w-[15%] p-[10px] border border-solid border-blue-800 bg-blue-800 text-white radius flex justify-center items-center transition-all duration-700' 
+                        : 'w-[15%] p-[10px] border border-solid border-blue-300 radius text-black flex justify-center items-center'
+                    }
+                >
+                    Completadas
+                </div>
+                <div 
+                    onClick={() => setStatus('pending')}
+                    style={{cursor:'pointer'}}
+                    className={status === 'pending' 
+                        ? 'w-[15%] p-[10px] border border-solid border-blue-800 bg-blue-800 text-white radius flex justify-center items-center transition-all duration-700' 
+                        : 'w-[15%] p-[10px] border border-solid border-blue-300 radius text-black flex justify-center items-center'
+                    }
+                >
+                    Pendientes
+                </div>
+            </div>
+        </div>
+        <div className='container-list'>
+            {tasks.map(({name,description,status}) => {
+                return <div className='h-auto min-h-[100px] min-w-[100px] radius flex row border-gray-200 border-solid shadow-sm hover:shadow-md transition-shadow duration-200'>
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                    <p>{status}</p>
+                </div>
+            })}
+        </div>
+    </div>
 }
 
 export default DashboardTareas
--- src/services/task-services.js
+++ src/services/task-services.js
@@ -1,9 +1,20 @@
 import { axiosQuery } from "../utils/axios"
 
-export const createTask = () => {
+export const getTasks = async (callback) => {
     try {
-        const response = await axiosQuery.post("/task"){
+        const response = await axiosQuery.get("/task");
+        if(response.status === 200){
+            return response.data
         }
+    }catch(err) {
+        console.log(err?.response?.data);
+        if(typeof callback === 'function'){callback(err?.response?.data)};
+    }
+}
+
+export const createTask = async () => {
+    try {
+        const response = await axiosQuery.post("/task")
 
     }
     catch {
